efc048b17f4e9106b491c90512a3533c116cd0b2
Banan358 <bragese@online.no>
2025-01-07 20:21:44 +0100

    plaserer bombene forskjellige steder
    det neste som skal gjøres er å fikse sånn at det går ann å klikke på feltene

--- script.js
+++ script.js
@@ -3,14 +3,19 @@
 
 import  { createBoard } from './script2.js'
 
-const BOARD_SIZE = 5
-const NUMBER_OF_MINES = 2
+const BOARD_SIZE = 10
+const NUMBER_OF_MINES = 5
 
 const board = (createBoard(BOARD_SIZE,NUMBER_OF_MINES))
 const brettElement = document.querySelector('.board')
-brettElement.style.setProperty('--size', BOARD_SIZE)
+const flaggIgjen = document.getElementById("flagg")
+
+console.log(board)
+
 board.forEach(row => {
     row.forEach(tile => {
         brettElement.append(tile.element)
     })
 })
+brettElement.style.setProperty('--size', BOARD_SIZE)
+flaggIgjen.textContent = NUMBER_OF_MINES
--- script2.js
+++ script2.js
@@ -8,8 +8,11 @@ const tile_status = {
 }
 
 
-export function createBoard(boardSize, numberOfMines){
+export function createBoard(boardSize, numberOfMines) {
     const board = []
+    const minePositions = getMinePositions(boardSize, numberOfMines)
+    console.log(minePositions)
+
     for (let x = 0; x < boardSize; x++) {
         const row = []
         for (let y = 0; y < boardSize; y++) {
@@ -19,7 +22,14 @@ export function createBoard(boardSize, numberOfMines){
             const tile = {
                 element,
                 x,
-                y
+                y,
+                mine: minePositions.some(positionMatch.bind(null, { x, y})),
+                get status() {
+                    return this.element.dataset.status
+                },
+                set status(value) {
+                    this.element.dataset.status = value
+                }
             }
             row.push(tile) 
         }
@@ -29,3 +39,28 @@ export function createBoard(boardSize, numberOfMines){
     return board
 }
 
+function getMinePositions(boardSize, numberOfMines) {
+    const positions = []
+
+    while (positions.length < numberOfMines) {
+        const position = {
+            x: randomNumber(boardSize),
+            y: randomNumber(boardSize),
+        }
+
+        if (!positions.some(positionMatch.bind(null, position))) {
+            positions.push(position)
+        }
+    }
+
+    return positions
+}
+
+function positionMatch(a, b) {
+    return a.x === b.x && a.y === b.y
+}
+
+// tilfeldig tall som må være heltall
+function randomNumber(size) {
+    return Math.floor(Math.random() * size)
+}

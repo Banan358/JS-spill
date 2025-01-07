5b5e2e453307b1e688cee5eaa61080c4c26cff70
Banan358 <bragese@online.no>
2025-01-07 21:08:52 +0100

    har lagt til funksjonene for når man klikker eller høyreklikker
    selve spilldelen og logikken er ferdig men må legge til tape og vinnefunksjoner og ekstra qol endringer

---
+++ funksjoner_og_logikk.js
@@ -0,0 +1,116 @@
+//logic
+
+export const TILE_STATUSES = {
+    HIDDEN: 'hidden',
+    MINE: 'mine',
+    NUMBER: 'number',
+    MARKED: 'marked',
+}
+
+
+export function createBoard(boardSize, numberOfMines) {
+    const board = []
+    const minePositions = getMinePositions(boardSize, numberOfMines)
+    console.log(minePositions)
+
+    for (let x = 0; x < boardSize; x++) {
+        const row = []
+        for (let y = 0; y < boardSize; y++) {
+            const element = document.createElement("div")
+            element.dataset.status = TILE_STATUSES.HIDDEN
+            
+            const tile = {
+                element,
+                x,
+                y,
+                mine: minePositions.some(positionMatch.bind(null, { x, y})),
+                get status() {
+                    return this.element.dataset.status
+                },
+                set status(value) {
+                    this.element.dataset.status = value
+                }
+            }
+            row.push(tile) 
+        }
+        board.push(row)
+    }
+
+    return board
+}
+
+
+export function markTile(tile) {
+    if (
+        tile.status !== TILE_STATUSES.HIDDEN && 
+        tile.status !== TILE_STATUSES.MARKED
+    ) {
+        return //gjør at koden ikke markerer hvis den er allerere klikket på, åpnet på en måte
+    }
+
+    if (tile.status === TILE_STATUSES.MARKED) {
+        tile.status = TILE_STATUSES.HIDDEN
+    } else {
+        tile.status = TILE_STATUSES.MARKED
+    }
+}
+
+export function revealTile(board, tile) {
+    if (tile.status !== TILE_STATUSES.HIDDEN) {
+        return
+    }
+    
+    if (tile.mine) {
+        tile.status = TILE_STATUSES.MINE
+        return
+    }
+    
+    //måten dette funker er at det er en array og .length sier hvor mange elementer det er i arrayet som i dette tilfellet er en rundt i alle retninger fra der man klikket
+    tile.status = TILE_STATUSES.NUMBER
+    const adjecentTiles = nearbyTiles(board, tile)
+    const mines = adjecentTiles.filter(t => t.mine)
+    if (mines.length === 0){
+        adjecentTiles.forEach(revealTile.bind(null, board))
+    } else {
+        tile.element.textContent = mines.length
+    }
+}
+
+function getMinePositions(boardSize, numberOfMines) {
+    const positions = []
+
+    while (positions.length < numberOfMines) {
+        const position = {
+            x: randomNumber(boardSize),
+            y: randomNumber(boardSize),
+        }
+
+        if (!positions.some(positionMatch.bind(null, position))) {
+            positions.push(position)
+        }
+    }
+
+    return positions
+}
+
+function positionMatch(a, b) {
+    return a.x === b.x && a.y === b.y
+}
+
+// tilfeldig tall som må være heltall
+function randomNumber(size) {
+    return Math.floor(Math.random() * size)
+}
+
+function nearbyTiles(board, { x, y }) {
+    const tiles = []
+
+    for (let xOffset = -1; xOffset <= 1; xOffset++) {
+        for (let yOffset = -1; yOffset <= 1; yOffset++) {
+            const tile = board[x + xOffset]?.[y + yOffset]
+            if (tile) tiles.push(tile)
+        }
+    }
+
+    return tiles 
+} // denne funksjonen tar først et element til venstre og tar en og en i høyden før den går over til neste rekke
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //hva som skjer når man klikker
 
 
-import  { createBoard } from './script2.js'
+import  { createBoard, markTile, TILE_STATUSES, revealTile } from './funksjoner_og_logikk.js'
 
 const BOARD_SIZE = 10
 const NUMBER_OF_MINES = 5
@@ -12,10 +12,29 @@ const flaggIgjen = document.getElementById("flagg")
 
 console.log(board)
 
+//click er venstreklikk og contextmenu er høyreklikk
 board.forEach(row => {
     row.forEach(tile => {
         brettElement.append(tile.element)
+        tile.element.addEventListener('click', () => {
+            revealTile(board, tile)
+        }) 
+        tile.element.addEventListener('contextmenu', e => {
+            e.preventDefault() //hindrer menyen som vanligvis kommer fra høyreklikk
+            markTile(tile)
+            listFlaggLeft()
+        }) 
     })
 })
 brettElement.style.setProperty('--size', BOARD_SIZE)
 flaggIgjen.textContent = NUMBER_OF_MINES
+
+
+function listFlaggLeft() {
+    const markedTilesCount = board.reduce((count, row ) => {
+        return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).
+        length
+    }, 0)
+
+    flaggIgjen.textContent = NUMBER_OF_MINES - markedTilesCount
+}
